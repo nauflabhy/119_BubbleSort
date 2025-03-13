4b0021a8f1b6683259e41729c77ee73b5d35dc1e
nauflabhy <n4uf4l.ku@gmail.com>
2025-03-13 16:53:09 +0700

    add procedur input and procedur bubble sort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -31,7 +31,7 @@ void inputData(){
         cin >> arr[i];
     }
 }
-void bubblesort()
+void bubbleSort()
 {
     int pass = 1; // step 1
     do
@@ -53,7 +53,23 @@ void bubblesort()
        } while (pass <= n-1); //step 5
        
 }
+void displayData()
+{
+    cout << endl;
+    cout << "==================" << endl;
+    cout << "Data Setelah Diurutkan" << endl;
+    cout << "==================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << i + 1 << " = " << arr[i] << endl;
+    }
+}
 int main()
 {
     inputData();
+    bubbleSort();
+    displayData();
+    return 0;
 }
